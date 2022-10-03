b1f0f2ae1d6be8c53e8c56668cc549afd5219478
Arif Esat Yılmaz <43481435+arifesat@users.noreply.github.com>
2022-10-03 20:24:17 +0300

    Add files via upload

--- yt-downloader-linux.py
+++ yt-downloader-linux.py
@@ -8,27 +8,33 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '256'
     }],
-    'outtmpl': "~/Downloads/Music/%(title)s.%(ext)s"
+    'outtmpl': "~/Downloads/Music/%(title)s.%(ext)s" #Music doownload location
     }
 ydl_opts_vid = {
-    'outtmpl': "~/Downloads/Video/%(title)s.%(ext)s",
+    'ignoreerrors' : 'True',
+    'format' : 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+    'outtmpl': "~/Downloads/Video/%(title)s.%(ext)s" #Video download location
 }
 def download_video(video):
     video = video
     yd.download([video])
     
-selection = int(input("Download Audio or Video?\nVideo: 1\nAudio: 2\n"))
-if selection == int(1) or selection == int(2):
-    video_link = input("Audio/Video Link: ")
-    if selection == int(1):
-        with yt_dlp.YoutubeDL(ydl_opts_vid) as yd:
-            video = video_link
-            download_video(video)
-    elif selection == int(2):
-        with yt_dlp.YoutubeDL(ydl_opts) as yd:
-            video = video_link
-            yd.download([video])    
+while True:
+    selection = int(input("Download Audio or Video?\nVideo: 1\nAudio: 2\nQuit: 0\n"))
 
+    if selection == int(1) or selection == int(2):
+        video_link = input("Audio/Video Link: ")
+        if selection == int(1):
+            with yt_dlp.YoutubeDL(ydl_opts_vid) as yd:
+                video = video_link
+                download_video(video)
+        elif selection == int(2):
+            with yt_dlp.YoutubeDL(ydl_opts) as yd:
+                video = video_link
+                yd.download([video])    
 
-else:
-    print("Enter a valid option.\n'1' for video, '2' for audio")
+    elif selection == int(0):
+        break
+
+    else:
+        print("Enter a valid option.\n'1' for video, '2' for audio")
